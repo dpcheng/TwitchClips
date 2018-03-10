e9c044ebd42e72d6478ba185a8797e7ab561391a
Dylan <dylan.patrick.cheng@gmail.com>
2018-03-10 01:37:32 -0500

    add embedded clip

--- src/containers/ClipMain.jsx
+++ src/containers/ClipMain.jsx
@@ -17,6 +17,14 @@ class ClipMain extends React.Component {
             // <div
             //     id="channel-main"
             // />
+            <iframe
+                src="https://clips.twitch.tv/embed?clip=SpeedyCaringOcelotDansGame"
+                height={700}
+                width={document.body.offsetWidth}
+                scrolling={false}
+                allowfullscreen={true}
+                preload=""
+            />
         )
     }
 }
