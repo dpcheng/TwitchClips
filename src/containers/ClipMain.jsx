840edbbf9bc05ab00c416bbfb9d295a758f1e2f2
Dylan <dylan.patrick.cheng@gmail.com>
2018-03-10 02:23:18 -0500

    pasting in whole doc strips out clip slugs and adds to state

--- src/containers/ClipMain.jsx
+++ src/containers/ClipMain.jsx
@@ -1,6 +1,13 @@
 import React from "react"
 
 class ClipMain extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            slugs: []
+        }
+    }
+
     componentDidMount() {
         // const options = {
         //     width: document.body.offsetWidth,
@@ -12,19 +19,35 @@ class ClipMain extends React.Component {
         // player.play()
     }
 
+    handleTextChange = (e) => {
+        const rawText = e.currentTarget.value
+        const myRe = /(?:https:\/\/clips.twitch.tv\/)(\S+)/g
+        var myArray;
+        while ((myArray = myRe.exec(rawText)) !== null) {
+            console.log(this.state.slugs.length, myArray[1])
+            this.setState({ slugs: this.state.slugs.push(myArray[1]) })
+        }
+    }
+
     render() {
         return (
             // <div
             //     id="channel-main"
             // />
-            <iframe
-                src="https://clips.twitch.tv/embed?clip=SpeedyCaringOcelotDansGame"
-                height={700}
-                width={document.body.offsetWidth}
-                scrolling={false}
-                allowfullscreen={true}
-                preload=""
-            />
+            <div>
+                <iframe
+                    src="https://clips.twitch.tv/embed?clip=SpeedyCaringOcelotDansGame"
+                    height={700}
+                    width={document.body.offsetWidth}
+                    scrolling={false}
+                    allowfullscreen={true}
+                    preload=""
+                />
+                <input
+                    type="text"
+                    onChange={this.handleTextChange}
+                />
+            </div>
         )
     }
 }
